94e2512b559cbbc7207e04702357b93dfdbc0214
anirvan90 <anirvan.awatramani@gmail.com>
2017-03-07 14:43:43 -0800

    videos render from db

--- public/app.js
+++ public/app.js
@@ -12,7 +12,7 @@ app.factory('posts', ['$http', function($http) {
   postItems.getAll = function() {
     return $http.get('/posts').then(function(data) {
       angular.copy(data, postItems.post);
-      console.log(postItems.post);
+      // console.log(postItems.post);
     });
   }
   return postItems;
@@ -21,21 +21,15 @@ app.controller('MainCtrl', [
   '$scope',
   "$sce",
   "posts",
-  function($scope, $sce, posts) {
+  function($scope, $sce, posts, post) {
     $scope.trust = function(url){
       return $sce.trustAsResourceUrl(url);
     }
     posts.getAll().then(function() {
-      console.log(postItems);
+      $scope.posts = posts.post.data;
+     $scope.urlString = $scope.posts[0].link;
+      // console.log($scope.posts);
     });
-    // $scope.posts = [
-    //   {title: 'Fernando Llorente vs Burnely', link:'https://www.youtube.com/embed/01j08KmeIyw', votes:0},
-    //   {title: 'Harry Kane vs Everton', link: 'https://www.youtube.com/embed/qVxnYmIitzE', votes:0},
-    //   {title: 'Leicester vs Hull', link: 'https://www.youtube.com/embed/70PWyDv93Bg', votes:0},
-    //   {title: 'Zlatan vs EVERYONE!', link: 'https://www.youtube.com/embed/kTI3lNTz4fA', votes:0},
-    //   {title: 'Sadio Mane vs Arsenal', link: 'https://www.youtube.com/embed/51woOX_Leog', votes:0}
-    // ];
-    $scope.urlString = $scope.posts[0].link;
     $scope.incrementVotes = function(post) {
       post.votes += 1;
     }
