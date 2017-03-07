bb50333038ad152464abcbce3c827c4a616c5fe8
anirvan90 <anirvan.awatramani@gmail.com>
2017-03-06 20:53:19 -0800

    play button works

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ app.controller('MainCtrl', [
     $scope.trust = function(url){
       return $sce.trustAsResourceUrl(url);
     }
-
     $scope.posts = [
       {title: 'Fernando Llorente vs Burnely', link:'https://www.youtube.com/embed/01j08KmeIyw', votes:0},
       {title: 'Harry Kane vs Everton', link: 'https://www.youtube.com/embed/qVxnYmIitzE', votes:0},
@@ -14,8 +13,12 @@ app.controller('MainCtrl', [
       {title: 'Zlatan vs EVERYONE!', link: 'https://www.youtube.com/embed/kTI3lNTz4fA', votes:0},
       {title: 'Sadio Mane vs Arsenal', link: 'https://www.youtube.com/embed/51woOX_Leog', votes:0}
     ];
+    $scope.urlString = $scope.posts[0].link;
     $scope.incrementVotes = function(post) {
       post.votes += 1;
+    }
+    $scope.playVideo = function(post) {
+      $scope.urlString = post.link;
     }
   }
 ]);
